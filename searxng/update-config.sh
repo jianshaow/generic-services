24009dc8d2bd4181ff96696d729314362c2f0b4a
Jianshao <787903+jianshaow@users.noreply.github.com>
2025-03-20 12:13:00 +0800

    fix issue

--- searxng/update-config.sh
+++ searxng/update-config.sh
@@ -3,7 +3,7 @@
 set -e
 
 secret_key=$(yq "." /usr/local/etc/searxng/settings.yml | jq -r ".server.secret_key")
-sudo sed "s/changeit/$secret_key/g" config/settings.yml > /usr/local/etc/searxng/settings.yml
+sed "s/changeit/$secret_key/g" config/settings.yml | sudo tee /usr/local/etc/searxng/settings.yml
 sudo cp config/uwsgi.ini /usr/local/etc/searxng/
 
 kubectl -nsearxng rollout restart deploy searxng
