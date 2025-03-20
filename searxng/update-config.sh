94bfce01d9a7f68546f7626fa8a942fa9e2e91bb
Jianshao <787903+jianshaow@users.noreply.github.com>
2025-03-20 12:08:12 +0800

    set fail and exit

--- searxng/update-config.sh
+++ searxng/update-config.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+set -e
+
 secret_key=$(yq "." /usr/local/etc/searxng/settings.yml | jq -r ".server.secret_key")
 sudo sed "s/changeit/$secret_key/g" config/settings.yml > /usr/local/etc/searxng/settings.yml
 sudo cp config/uwsgi.ini /usr/local/etc/searxng/
