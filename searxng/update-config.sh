1e3bd0699f98906d76a0b5ab5a2667ecc5bcfe6b
Jianshao <787903+jianshaow@users.noreply.github.com>
2025-03-20 12:02:21 +0800

    restart deplyment

--- searxng/update-config.sh
+++ searxng/update-config.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
 secret_key=$(yq "." /usr/local/etc/searxng/settings.yml | jq -r ".server.secret_key")
-sed "s/changeit/$secret_key/g" config/settings.yml > /usr/local/etc/searxng/settings.yml
-cp config/uwsgi.ini /usr/local/etc/searxng/
+sudo sed "s/changeit/$secret_key/g" config/settings.yml > /usr/local/etc/searxng/settings.yml
+sudo cp config/uwsgi.ini /usr/local/etc/searxng/
+
+kubectl -nsearxng rollout restart deploy searxng
